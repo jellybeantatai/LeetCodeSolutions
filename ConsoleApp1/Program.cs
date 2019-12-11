41f7a95741a52a46f9b31d6eb7d3a6524b785221
Abhinaba <abhinabakar@gmail.com>
2019-12-12 00:03:23 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    public class ListNode
+    {
+        public long val;
+        public ListNode next;
+
+        public ListNode(long i)
+        {
+            val = i;
+            next = null;
+        }
+
+        public void Print()
+        {
+            Console.Write("|" + val + "| -> ");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+
+        public void AddToEnd(long val)
+        {
+            if (next == null)
+                next = new ListNode(val);
+            else
+                next.AddToEnd(val);
+        }
+    }
+
+    public class MyList
+    {
+        public ListNode headNode;
+
+        public MyList()
+        {
+            headNode = null;
+        }
+
+        public void AddToEnd(long val)
+        {
+            if(headNode == null)
+            {
+                headNode = new ListNode(val);
+            }
+            else
+            {
+                headNode.AddToEnd(val);
+            }
+        }
+
+        public void AddToStart(long val)
+        {
+            if(headNode == null)
+            {
+                headNode = new ListNode(val);
+            }
+            else
+            {
+                var temp = headNode;
+                headNode = new ListNode(val);
+                headNode.next = temp;
+            }
+        }
+
+        public void Print()
+        {
+            if(headNode!= null)
+            {
+                headNode.Print();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListNode l1 = new ListNode(2);
+            l1.AddToEnd(4);
+            l1.AddToEnd(3);
+
+            ListNode l2 = new ListNode(5);
+            l2.AddToEnd(6);
+            l2.AddToEnd(4);
+
+            Solution1 sol = new Solution1();
+
+            ListNode result = sol.AddTwoNumbers(l1, l2);
+
+            result.Print();
+        }
+    }
+}
---
+++ ConsoleApp1/Solution1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    public class Solution1
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode currentNode1 = l1;
+            long multiplier1 = 1;
+            long firstNumber = 0;
+            while (currentNode1 != null)
+            {
+                firstNumber += (long)currentNode1.val * multiplier1;
+                currentNode1 = currentNode1.next;
+                multiplier1 *= 10;
+            }
+
+            ListNode currentNode2 = l2;
+            long multiplier2 = 1;
+            long secondNumber = 0;
+            while (currentNode2 != null)
+            {
+                secondNumber += (long)currentNode2.val * multiplier2;
+                currentNode2 = currentNode2.next;
+                multiplier2 *= 10;
+            }
+
+            long sum = firstNumber + secondNumber;
+
+            if (sum == 0)
+            {
+                return new ListNode(0);
+            }
+
+            long digitsInSum = NumberOfDigitsInANumber(sum);
+
+            ListNode finalList = null;
+
+            while (digitsInSum != 0)
+            {
+                long divisor = (long)(Math.Pow(10, (digitsInSum - 1)));
+                finalList = AddToBeginningOfList(finalList, sum / divisor);
+                sum = sum % divisor;
+                digitsInSum--;
+            }
+
+            return finalList;
+        }
+
+
+        public static long NumberOfDigitsInANumber(long number)
+        {
+            long count = 0;
+            long divisor = 1;
+            while ((number / divisor) != 0)
+            {
+                count++;
+                divisor *= 10;
+            }
+
+            return count;
+        }
+
+        public static ListNode AddToBeginningOfList(ListNode n, long val)
+        {
+            ListNode temp = n;
+            ListNode HeadNode;
+            if (n == null)
+            {
+                HeadNode = new ListNode((int)val);
+            }
+            else
+            {
+                HeadNode = new ListNode((int)val);
+                HeadNode.next = temp;
+            }
+            return HeadNode;
+        }
+    }
+
+}
